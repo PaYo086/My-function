79dccd1de3222b6ba794d3ed86e7d4879f751c9f
Po-Yu Lin <38680719+PaYo086@users.noreply.github.com>
2020-03-11 21:31:02 +0800

    Update part_regre_plot.r

--- part_regre_plot.r
+++ part_regre_plot.r
@@ -3,7 +3,7 @@ part.regre.plot <- function(v, y = 1, x = 2, line = TRUE, return.lm = FALSE){
   if (is.numeric(x) & is.numeric(y)){
     y.formula <- as.formula(paste0(colnames(v)[y], " ~ ."))
     x.formula <- as.formula(paste0(colnames(v)[x], " ~ ."))
-    plot(resid(lm(y.formula, data = v[,-x])) ~ resid(lm(x.formula, data = v[,-y])), ylab = paste0("Residual of ", colnames(v)[y]), xlab = paste0("Residual of ", colnames(v)[x]))
+    plot(resid(lm(y.formula, data = v[,-x])) ~ resid(lm(x.formula, data = v[,-y])), ylab = paste0("Residual of ", colnames(v)[y]), xlab = paste0("Residual of ", colnames(v)[x]), asp = 1)
     if (line){
       lm.v <- lm(resid(lm(y.formula, data = v[,-x])) ~ resid(lm(x.formula, data = v[,-y])))
       abline(lm.v, col = "red")
@@ -11,7 +11,7 @@ part.regre.plot <- function(v, y = 1, x = 2, line = TRUE, return.lm = FALSE){
   } else{
     order <- c(which(colnames(v) == y), which(colnames(v) == x))
     colnames(v)[order] <- c("res.v", "exp.v")
-    plot(resid(lm(res.v ~ ., data = v[, -order[2]])) ~ resid(lm(exp.v ~ ., data = v[, -order[1]])), ylab = paste0("Residual of ", y), xlab = paste0("Residual of ", x))
+    plot(resid(lm(res.v ~ ., data = v[, -order[2]])) ~ resid(lm(exp.v ~ ., data = v[, -order[1]])), ylab = paste0("Residual of ", y), xlab = paste0("Residual of ", x), asp = 1)
     if (line){
       lm.v <- lm(resid(lm(res.v ~ ., data = v[, -order[2]])) ~ resid(lm(exp.v ~ ., data = v[, -order[1]])))
       abline(lm.v, col = "red")
